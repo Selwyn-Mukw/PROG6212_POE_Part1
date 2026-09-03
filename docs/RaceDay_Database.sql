0d764bbbe8b9212e6fa7d416988aa0931a4a65ad
Selwyn Mukwevho <ST10465950@rcconnect.edu.za>
2026-09-03 19:28:01 +0200

    Create database in sql
    Create the table for users

---
+++ docs/RaceDay_Database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [USER]
+(
+    UserID INT IDENTITY(1,1),
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    PhoneNumber VARCHAR(20),
+    PasswordHash VARCHAR(255),
+    Role VARCHAR(20),
+    ProfilePictureURL VARCHAR(500),
+    CreatedAt DATETIME2
+);
