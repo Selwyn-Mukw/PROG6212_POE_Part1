3b9890bbe59f68d2a87bde6c457ad2fcec7d3b7d
Selwyn Mukwevho <ST10465950@rcconnect.edu.za>
2026-09-03 19:42:13 +0200

    Create Category table

--- docs/RaceDay_Database.sql
+++ docs/RaceDay_Database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE RaceDay;
+USE RaceDay;
 CREATE TABLE [USER]
 (
     UserID INT IDENTITY(1,1),
@@ -15,4 +17,28 @@ CREATE TABLE [EVENT TYPE]
 (
     EventTypeID INT IDENTITY(1,1),
     TypeName VARCHAR(50)
+);
+
+CREATE TABLE [EVENT]
+(
+    EventID INT IDENTITY(1,1),
+    OrganiserID INT,
+    EventTypeID INT,
+    EventName VARCHAR(100),
+    Description VARCHAR(500),
+    EventDate DATETIME2,
+    Location VARCHAR(150),
+    Distance DECIMAL(6,2),
+    BannerImageURL VARCHAR(500)
+);
+
+CREATE TABLE [CATEGORY]
+(
+    CategoryID INT IDENTITY(1,1),
+    EventID INT,
+    CategoryName VARCHAR(100),
+    CategoryType VARCHAR(50),
+    MinimumAge INT,
+    MaximumAge INT,
+    CategoryDistance DECIMAL(6,2)
 );
